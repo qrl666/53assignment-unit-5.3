bacc1dd1f2b698adffae87c4a95d26772bf86526
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-01-12 10:33:55 -0600

    first try at addToCollection fxn, seems to work for first album

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+}
+collection.push(newAlbum)
+return newAlbum
+}
+
+console.log(addToCollection("Enter the Wu-Tang", "Wu-Tang Clan", "1993"))
+console.log(collection)
